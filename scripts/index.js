3de3b8666f46412c6b6c12e31bd3fcc6b99916dd
Mariya Fedorova <iguanafeelsagape@gmail.com>
2023-04-27 00:27:48 +0300

    PG5 variant2_7 270423

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 //Элементы секции EditProfile
 const editProfilePopupOpenBtn = document.querySelector('.edit-button');
 const editProfilePopup = document.querySelector('.popup_type_edit-profile');
-const editProfilePopupCloseBtn = editProfilePopup.querySelector('.popup__close-btn_type_edit-profile');
 const editProfileNameField = editProfilePopup.querySelector('.popup__field_type_name');
 const editProfileJobField = editProfilePopup.querySelector('.popup__field_type_job');
 const personName = document.querySelector('.info__name');
@@ -14,16 +13,17 @@ const addContentPlaceField = formAddContentElement.querySelector('.popup__field_
 const addContentAdressField = formAddContentElement.querySelector('.popup__field_type_adress');
 const addContentPopupOpenBtn = document.querySelector('.add-button');
 const addContentPopup = document.querySelector('.popup_type_add-content');
-const addContentPopupCloseBtn = document.querySelector('.popup__close-btn_type_add-content');
 const addContentEditBtn = addContentPopup.querySelector('.popup__edit-btn_type_add-content');
 //Элементы секции создания карточки createCardsElement
 const cardsGrid = document.querySelector('.elements');
 const cardsTemplate = document.getElementById('cards-template');
 //Элементы секции увеличения изображения zoomImage
 const zoomImagePopup = document.querySelector('.popup_type_zoom-image');
-const zoomImagePopupCloseBtn = zoomImagePopup.querySelector('.popup__close-btn_type_zoom-image');
 const zoomImagePopupPicture = zoomImagePopup.querySelector('.popup__image');
 const zoomImagePopupTitle = zoomImagePopup.querySelector('.popup__title_type_zoom-image');
+//Элементы для закрытия popup
+const closeBtns = document.querySelectorAll('.popup__close-btn');
+const popups = document.querySelectorAll('.popup');
 
 //Открыть popup
 const openPopup = (popupElement) => {
@@ -31,11 +31,15 @@ const openPopup = (popupElement) => {
 };
 
 //Закрыть popup
-const closePopup = (popupElement) => {
-    popupElement.classList.remove('popup_open');
+closePopup = () => {
+    for (let i = 0; i < popups.length; ++i) {
+        if (popups[i].classList.contains('popup_open')) {
+            popups[i].classList.remove('popup_open')
+        }
+    }
 };
-const closePopupByBtn = (closeBtn, popupElement) => {
-    closeBtn.addEventListener('click', (event) => { closePopup(popupElement) });
+for (let i = 0; i < closeBtns.length; ++i) {
+    closeBtns[i].addEventListener('click', closePopup)
 };
 
 // Редактирование персональных данных о пользователе
@@ -45,13 +49,12 @@ const openEditProfilePopup = () => {
     openPopup(editProfilePopup);
 };
 editProfilePopupOpenBtn.addEventListener('click', openEditProfilePopup);
-closePopupByBtn(editProfilePopupCloseBtn, editProfilePopup);
 
 function handleEditProfileFormSubmit(evt) {
     evt.preventDefault();
     personName.textContent = editProfileNameField.value;
     personJob.textContent = editProfileJobField.value;
-    closePopup(editProfilePopup);
+    editProfilePopup.classList.remove('popup_open');
 }
 formEditProfileElement.addEventListener('submit', handleEditProfileFormSubmit);
 
@@ -87,7 +90,6 @@ const createCardsElement = (card) => {
         zoomImagePopupPicture.src = cardImage.src;
         zoomImagePopupTitle.textContent = cardTitle.textContent;
     });
-    closePopupByBtn(zoomImagePopupCloseBtn, zoomImagePopup);
 
     return cardElement;
 };
@@ -98,13 +100,13 @@ initialCards.forEach((userCard) => {
 
 //Добавление новой карточки
 addContentPopupOpenBtn.addEventListener('click', (event) => { openPopup(addContentPopup) });
-closePopupByBtn(addContentPopupCloseBtn, addContentPopup);
+
 const handleAddContentFormSubmit = (evt) => {
     evt.preventDefault();
     const newCard = { name: addContentPlaceField.value, link: addContentAdressField.value };
     const newCardElement = createCardsElement(newCard);
     cardsGrid.prepend(newCardElement);
-    closePopup(addContentPopup);
+    addContentPopup.classList.remove('popup_open');
     formAddContentElement.reset();
 };
 formAddContentElement.addEventListener('submit', handleAddContentFormSubmit);
